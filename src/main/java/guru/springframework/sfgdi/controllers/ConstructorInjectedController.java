0eb4f3ac935583f961bf75cf5ddee0c98056c14c
ronsahota <ronsahota@hotmail.com>
2020-02-09 15:29:01 -0500

    Dependency injection using spring framework.

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,10 +1,14 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
+    //Optional @Autowired not needed as of spring 4
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -1,5 +1,8 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.stereotype.Service;
+//@Service - tells Spring it is a spring management component and to bring it to the Spring context.
+@Service
 public class GreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
